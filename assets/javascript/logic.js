fb565603c3336142933315e8de0d419500e84ab8
KB Total Recall <kanwar@gmail.com>
2019-03-08 19:44:54 -0800

    Add train Arriaval and Minutes away logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -20,16 +20,13 @@ $(function () {
     var trainName = '';
     var destination = '';
     var frequency = '';
-    var nextArrival = 0;
-    var minutesAway = '';
+    //var trainTime ='';
+   //s var nextArrival = 0;
+    // var minutesAway = '';
+   // var firstTrainTime='';
+   //var nextTrainTime ='';
 
-    function TrainAway(currentTime){
-        var a = moment([2007, 0, 29]);
-        var b = moment([2007, 0, 28]);
-        a.diff(b, 'days') 
-        //var currentTime =(moment().now());
-        //return currentTime;
-    }
+   
 
 
     $(document).on("click", "button[type=submit]", function (event) {
@@ -38,37 +35,56 @@ $(function () {
         trainName = $("#trainName").val().trim();
         destination = $("#destination").val().trim();
         frequency = $("#frequency").val().trim();
-        minutesAway = moment().toNow() +frequency;
-       // var currentTime = TrainAway();
-        //console.log(currentTime);
+        trainTime = $("#trainTime").val().trim();
+
+        
+        var firstTrainTime = moment($("#trainTime").val().trim(),"HH:mm").format("HH:mm");;
+
 
         var newElement = {
             trainName: trainName,
             destination: destination,
             frequency: frequency,
-            minutesAway: minutesAway,
+            trainTime: firstTrainTime,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         };
-        console.log($("#trainName").val());
+       // console.log(" first Train Time" ,firstTrainTime);
 
         database.ref().push(newElement);
+
+        // Clear the Input Fields for Trains
         $("#trainName").val("");
         $("#destination").val("");
         $("#frequency").val("");
-        $("#minutesAway").val(""); 
+        $("#nextTrainTime").val(""); 
+        trainTime='';
+        firstTrainTime='';
+        
     });
 
     // }
     database.ref().on("child_added", function (snapshot) {
-        var sv = snapshot.val();
 
+           var sv = snapshot.val();
+           var now = new Date();
+
+           //console.log(now.getMinutes());
+           //---------------------------Time----------------------//
+
+           var firstTrainTime = moment.unix(parseInt(sv.trainTime)).format("hh:mm");
+           var Traindiff = moment(now, 'minutes').diff(parseInt(firstTrainTime),'minutes');
+           var timeRemaining = Traindiff % sv.frequency;
+           var minutesRemaining = (sv.frequency - timeRemaining);
+           var nextTrain = moment().add(minutesRemaining, 'minutes').format('h:mm a');
+         //  console.log(firstTrainTime, Traindiff, timeRemaining, minutesRemaining);
+         
+           //------------   Time to next Train above------------------//
+       
         $("#trainTable").append("<tr><td scope='col'>" + sv.trainName + "</td>" +
             "<td scope='col'>" + sv.destination + "</td>" +
             "<td scope='col'>" + sv.frequency + "</td>" +
-            "<td scope='col'>" + " " + "</td>" +
-            "<td scope='col'>" + '' + "</td>" +
-            "<td scope='col'>" + sv.minutesAway + "</td> ></tr>");
-
+            "<td scope='col'>" + nextTrain + "</td>" +
+            "<td scope='col'>" + minutesRemaining + "</td> ></tr>");
 
     });
 
